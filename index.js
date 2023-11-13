1b91357d3803d13125bf131993b1945d99099a1d
Lukas Hermann <hey@lukashermann.dev>
2023-11-13 16:24:02 +0100

    feat: add `parseToken` and `isValidToken` + some tests

--- index.js
+++ index.js
@@ -1,29 +1,71 @@
 const jwt = require('jsonwebtoken')
 
-const encodedSchema = {
-  planId: { type: Number, default: 0 },
-  email: { type: String, default: null },
-  uid: { type: String, default: null },
-}
+/**
+ * type TokenPayload = Record<string, any>
+ */
 
-const parsedSchema = {
-  planId: { type: Number, default: 0 },
-  email: { type: String, default: null },
-  uid: { type: String, default: null },
-  exp: { type: String, default: null },
-  token: { type: String, default: null },
-  valid: { type: String, default: false },
-}
+/**
+ * type ParsedToken = TokenPayload & {
+ *   iat: Date
+ *   exp: Date
+ *   isValid: boolean
+ * }
+ */
 
-function signToken (payload, privateKey, expiresIn = '1 month') {
+/**
+ * Signs a payload with the provided private key to create a JSON Web Token (JWT).
+ *
+ * @param {TokenPayload} payload - The payload to be signed.
+ * @param {string} privateKey - The private key used for signing the token.
+ * @param {string|Date} expiresIn - The duration for which the token is valid (default is '1 month').
+ * @return {string} - The signed JWT.
+ */
+function signToken (payload, privateKey, expiresIn = '1m') {
+  if (expiresIn instanceof Date ) expiresIn = Math.floor((expiresIn.getTime() - Date.now()) / 1000)
   return jwt.sign(
-    applyTokenSchema(payload, encodedSchema),
+    payload,
     _parseKey(privateKey),
     { algorithm: 'RS256', expiresIn },
   )
 }
 
-function validateToken (tokenString, publicKey) {
+/**
+ * Parse JWT Token
+ * @param  {string} tokenString
+ * @param  {string} publicKey
+ * @return {ParsedToken}
+ */
+function parseToken (tokenString, publicKey) {
+  const parsed = jwt.verify(tokenString, _parseKey(publicKey), { algorithms: ['RS256'], ignoreExpiration: true })
+  parsed.exp = new Date(parsed.exp * 1000)
+  parsed.iat = new Date(parsed.iat * 1000)
+  parsed.isValid = new Date() <= parsed.exp
+  return parsed
+}
+
+/**
+ * Checks if a JWT token is valid (signature validates and is note expired)
+ * @param  {string} tokenString
+ * @param  {string} publicKey
+ * @return {boolean}
+ */
+function isValidToken (tokenString, publicKey) {
+  try {
+    jwt.verify(tokenString, _parseKey(publicKey), { algorithms: ['RS256'] })
+    return true
+  } catch (err) {
+    return false
+  }
+}
+
+/**
+ * Validates a JSON Web Token (JWT) using the provided public key.
+ *
+ * @param {string} tokenString - The JWT string to be validated.
+ * @param {string} publicKey - The public key used to verify the token.
+ * @return {any} - An object representing the decoded token, including a 'valid' flag indicating the token's validity.
+ */
+function validateToken_DEPRECATED (tokenString, publicKey) {
   let decoded
 
   try {
@@ -37,17 +79,9 @@ function validateToken (tokenString, publicKey) {
   }
 
   return {
-    ...applyTokenSchema(decoded),
+    ...decoded,
     token: tokenString,
   }
-}
-
-function applyTokenSchema (payload = {}, schema = parsedSchema) {
-  const result = {}
-  for (const key in schema) {
-    result[key] = payload[key] || schema[key].default
-  }
-  return result
 }
 
 /**
@@ -72,15 +106,11 @@ function _parseKey (key) {
   return formattedKey
 }
 
-exports.signToken = signToken
-exports.validateToken = validateToken
-exports.applyTokenSchema = applyTokenSchema
-exports._parseKey = _parseKey
-
 module.exports = {
   signToken,
-  validateToken,
-  applyTokenSchema,
+  parseToken,
+  isValidToken,
+  validateToken: validateToken_DEPRECATED,
   _parseKey,
 }
 
--- tests/index.test.js
+++ tests/index.test.js
@@ -1,5 +1,9 @@
 const { expect } = require('chai')
-const { _parseKey } = require('../index.js')
+const { _parseKey, signToken, parseToken } = require('../index.js')
+const { readFileSync } = require('fs')
+
+const JWT_PRIVATE_KEY = readFileSync(__dirname + '/jwt-fixture-2048-RS256.key', { encoding: 'utf8' })
+const JWT_PUBLIC_KEY = readFileSync(__dirname + '/jwt-fixture-2048-RS256.key.pub', { encoding: 'utf8' })
 
 describe('_parseKey', () => {
   const mockPublicKey = `-----BEGIN PUBLIC KEY-----
@@ -47,8 +51,46 @@ vceYTMfN
   })
 })
 
-// describe('signToken', () => {
-// })
+describe('signToken', () => {
+  const activeUntil = new Date(new Date().setMonth(new Date().getMonth() + -1))
+  const samplePayload = {
+    n: 'Pro',
+    img: 'ic-pro-plan.png',
+    lim: { s: 4, l: 2, r: -1, d: 20, t: 50, m: 50 },
+    prm: ['LICENSED', 'API_ACCESS'],
+    t: 'MYNK9OdTESfbLPaj8ioA95cWFAt1',
+  }
 
-// describe('validateToken', () => {
-// })
+  test('sign payload', () => {
+    const token = signToken(samplePayload, JWT_PRIVATE_KEY, activeUntil)
+    expect(typeof token).to.equal('string')
+  })
+})
+
+describe('parseToken', () => {
+  test('parse valid token', () => {
+    const future = new Date(new Date().setMonth(new Date().getMonth() + 1))
+    const token = signToken({ foo: 'bar' }, JWT_PRIVATE_KEY, future)
+    const parsedToken = parseToken(token, JWT_PUBLIC_KEY)
+    expect(typeof parsedToken).to.equal('object')
+    expect(parsedToken.foo).to.equal('bar')
+    expect(parsedToken.exp.getTime()).to.equal(future.setMilliseconds(0))
+    expect(parsedToken.isValid).to.equal(true)
+  })
+
+  test('parse expired token', () => {
+    const past = new Date(new Date().setMonth(new Date().getMonth() - 1))
+    const token = signToken({ foo: 'bar' }, JWT_PRIVATE_KEY, past)
+    const parsedToken = parseToken(token, JWT_PUBLIC_KEY)
+    expect(typeof parsedToken).to.equal('object')
+    expect(parsedToken.foo).to.equal('bar')
+    expect(parsedToken.exp.getTime()).to.equal(past.setMilliseconds(0))
+    expect(parsedToken.isValid).to.equal(false)
+  })
+
+  test('parse token with invalid signature', () => {
+    const token = signToken({ foo: 'bar' }, JWT_PRIVATE_KEY)
+    const fn = () => parseToken(token + 'XXX', JWT_PUBLIC_KEY)
+    expect(fn).to.throw()
+  })
+})
